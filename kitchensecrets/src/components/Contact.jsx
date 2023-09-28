68f95c7564ddccde3d81d7f95d7e703a424cc608
berialx <emailbereket@gmail.com>
2023-09-28 17:00:16 +0300

    Contact us page

--- kitchensecrets/src/components/Contact.jsx
+++ kitchensecrets/src/components/Contact.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import "../Styles/Contact.css"
 
 const Contact = () => {
   return (
-    <div>Contact Us Nowwwww</div>
+    <div className='contact-us-container'>
+      <div><h1>Your message is important to us!</h1></div>
+      <div>
+        <form action="https://api.web3forms.com/submit" method="POST">
+          <input type="hidden" name="apikey" value="912bbd8e-862b-44a6-a1da-777064706a0f" />
+          <label for="first-name">Enter Your Name: <input type="text" name="name" placeholder="Insert your name here" required /></label>
+          <label for="first-name">Enter Your Email: <input type="email" name="email" placeholder="Insert your email here" required /></label>
+          <label for="first-name">Enter Your Message: <textarea name="message" placeholder="Insert your message here" required></textarea></label>
+          <div class="h-captcha" data-captcha="true"></div>
+          <button type="submit">Submit Form</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
-export default Contact
+export default Contact;
